0d041d3a92ef48ddac1618a5a15517f51dfb9f5c
Luis Fernando <el-fen@hotmail.com>
2021-01-11 23:33:39 -0600

    'v1'

---
+++ vistas/inicio.php
@@ -0,0 +1,28 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['usuario'])) {
+	include "header.php";
+	?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12" style="text-align: center;">
+				<div class="jumbotron">
+				
+					<hr class="my-4">
+					<p><img src="../img/logo.png" class="img-thumbnail" width="300px" /></p>
+					
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+
+	<?php
+	include "footer.php"; 
+} else {
+	header("location:../index.php");
+}
+?>
